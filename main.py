bcf06eaba9bc01f4bf06792e103c9216c11075a8
h1ten2208 <h1ten2208@mail.ru>
2026-03-09 19:55:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+r = True
 a = int(input())
 b = int(input())
 x = 1
+x1 = 0
 while x < b:
+  Sx = x + x1
   x1 = x
-  x += x1
-  print(x)
-  
+  x = Sx
+  if x >= a and x <= b:
+    r = False
+    print(x)
+if r:
+  print("В заданном диапазоне нет чисел Фибоначчи")
